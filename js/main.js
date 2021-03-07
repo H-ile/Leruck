0f8d0f6b87e0214143bd6253f511753d6e6251cd
H-ile <achille.monteil@limayrac.fr>
2021-03-07 19:49:37 +0100

    v1

--- js/main.js
+++ js/main.js
@@ -253,7 +253,7 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown, #date-countdown2').countdown('2021/03/05', function(event) {
+		$('#date-countdown, #date-countdown2').countdown('2021/03/27', function(event) {
 		  var $this = $(this).html(event.strftime(''
 		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
